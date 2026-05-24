d57b13ca561d54c33b9f67c4ae16b00ead935cb5
Bruno Schumacher F. Souza <brunoschumacherfsouza@gmail.com>
2026-05-24 04:43:08 -0300

    first commit

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+import type { JSX as ReactJSX } from 'react';
+
+declare global {
+  namespace JSX {
+    type Element = ReactJSX.Element;
+    type ElementType = ReactJSX.ElementType;
+    interface IntrinsicElements extends ReactJSX.IntrinsicElements {}
+  }
+}
+
+export {};
